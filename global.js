18995d7e186843d3df12489494ef802b449a5268
Lex1ner1 <luke@zezamii.com>
2023-02-08 16:59:27 +1000

    edit global logged in handler

--- global.js
+++ global.js
@@ -26,6 +26,17 @@ function handleSignOut() {
     window.location.replace("./log-in")
     // Sign-out successful.
 }
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        var uid = user.uid
+        console.log(uid, user)
+    } else {
+        console.log("not logged in!")
+    }
+})
 if (logoutButton) {
     logoutButton.addEventListener("click", handleSignOut)
 }
