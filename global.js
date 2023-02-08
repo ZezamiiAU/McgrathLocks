dcc8f46b421c39d81a2720bf24803fca647ae879
Lex1ner1 <luke@zezamii.com>
2023-02-08 14:11:21 +1000

    code format update

--- global.js
+++ global.js
@@ -1,31 +1,31 @@
 var webFlowAuth = {
-  loginPath: "/log-in",
-  loginRedirectPath: "/dashboard",
-  signupPath: "/sign-up",
-  signupRedirectPath: "/dashboard",
-  logoutRedirectPath: "/",
-  firebaseConfig: {
-    apiKey: "AIzaSyASFSsHovAAFE6bKzPt2pprKFa1pF7FzYs",
-    authDomain: "zezamiittlockintegration.firebaseapp.com",
-    projectId: "zezamiittlockintegration",
-    storageBucket: "zezamiittlockintegration.appspot.com",
-    messagingSenderId: "399470193827",
-    appId: "1:399470193827:web:2da9152802f29d3b9e2316",
-    measurementId: "G-3JX478JX8X",
-  },
-};
+    loginPath: "/log-in",
+    loginRedirectPath: "/dashboard",
+    signupPath: "/sign-up",
+    signupRedirectPath: "/dashboard",
+    logoutRedirectPath: "/",
+    firebaseConfig: {
+        apiKey: "AIzaSyASFSsHovAAFE6bKzPt2pprKFa1pF7FzYs",
+        authDomain: "zezamiittlockintegration.firebaseapp.com",
+        projectId: "zezamiittlockintegration",
+        storageBucket: "zezamiittlockintegration.appspot.com",
+        messagingSenderId: "399470193827",
+        appId: "1:399470193827:web:2da9152802f29d3b9e2316",
+        measurementId: "G-3JX478JX8X"
+    }
+}
 // initialise firebase Auth & Firestore
-firebase.initializeApp(webFlowAuth.firebaseConfig);
-const db = firebase.firestore();
+firebase.initializeApp(webFlowAuth.firebaseConfig)
+const db = firebase.firestore()
 
 // Finalising the global config.
 
-var logoutButton = document.getElementById("logoutButton");
+var logoutButton = document.getElementById("logoutButton")
 
 function handleSignOut() {
-  window.location.replace("./log-in");
-  // Sign-out successful.
+    window.location.replace("./log-in")
+    // Sign-out successful.
 }
 if (typeof logoutButton !== null) {
-  logoutButton.addEventListener("click", handleSignOut);
+    logoutButton.addEventListener("click", handleSignOut)
 }
--- pages/signin.js
+++ pages/signin.js
@@ -1,43 +1,42 @@
 // Login Function
-var loginButton = document.getElementById("loginButton");
+var loginButton = document.getElementById("loginButton")
 loginButton.addEventListener("click", function (e) {
-  e.preventDefault();
-  authenticate();
-});
+    e.preventDefault()
+    authenticate()
+})
 
 function authenticate() {
-  const emailInput = document.getElementById("loginEmail").value;
-  const passwordInput = document.getElementById("loginPassword").value;
+    const emailInput = document.getElementById("loginEmail").value
+    const passwordInput = document.getElementById("loginPassword").value
 
-  if (!emailInput || !passwordInput) {
-    document.getElementById("loginErrorMessage").innerHTML =
-      "Email or Password is missing.";
-    return;
-  }
+    if (!emailInput || !passwordInput) {
+        document.getElementById("loginErrorMessage").innerHTML = "Email or Password is missing."
+        return
+    }
 
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(emailInput, passwordInput)
-    .then((s) => {
-      var idRef = db
-        .collection("users")
-        .get()
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(emailInput, passwordInput)
         .then((s) => {
-          console.log(s);
-        });
-      return s;
-    })
-    .then(function (user) {
-      console.log(user.user.uid);
-      user.user.getIdToken().then((t) => {
-        console.log(t);
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-      loginErrors.forEach(function (el) {
-        el.innerText = error.message;
-        el.style.display = "block";
-      });
-    });
+            var idRef = db
+                .collection("users")
+                .get()
+                .then((s) => {
+                    console.log(s)
+                })
+            return s
+        })
+        .then(function (user) {
+            console.log(user.user.uid)
+            user.user.getIdToken().then((t) => {
+                console.log(t)
+            })
+        })
+        .catch(function (error) {
+            console.log(error)
+            loginErrors.forEach(function (el) {
+                el.innerText = error.message
+                el.style.display = "block"
+            })
+        })
 }
