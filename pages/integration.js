83b86c3b84b64c01ab73ce5a99ff8dd5550caabc
Lex1ner1 <luke@zezamii.com>
2023-03-14 11:59:41 +1000

    md5 path change

--- pages/integration.js
+++ pages/integration.js
@@ -1,8 +1,8 @@
-// ;<html>
-//     <head>
-//         <script type='text/javascript' src='./md5.js'></script>
-//     </head>
-// </html>
+;<html>
+    <head>
+        <script type='text/javascript' src='McgrathLocks\node_modules\md5\md5.js'></script>
+    </head>
+</html>
 
 const credentialRMSSetup = async () => {
     const rmsErrorMessage = document.getElementById("rmsErrorMessage")
