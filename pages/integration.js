302cb37c409c48879e9dfa57753a2728c93f471a
Lex1ner1 <luke@zezamii.com>
2023-03-21 16:10:00 +1000

    remove comments

--- pages/integration.js
+++ pages/integration.js
@@ -27,21 +27,6 @@ const uploadFileSetup = async () => {
     }
 }
 
-// RMS Save Button  variables
-
-// // Create a root reference
-// var storageRef = firebase.storage().ref();
-
-// // Create a reference to 'mountains.jpg'
-// var mountainsRef = storageRef.child('mountains.jpg');
-
-// // Create a reference to 'images/mountains.jpg'
-// var mountainImagesRef = storageRef.child('images/mountains.jpg');
-
-// // While the file names are the same, the references point to different files
-// mountainsRef.name === mountainImagesRef.name;           // true
-// mountainsRef.fullPath === mountainImagesRef.fullPath;   // false
-
 async function credentialRMSSetup() {
     const rmsErrorMessage = document.getElementById("rmsErrorMessage")
     //RMS Cloud Credentials Variables
